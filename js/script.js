2dc3dcd25c4a8ae27afcda02192e4ae81cdfa28a
Andrea-Baggio <andreabaggio800@gmail.com>
2022-10-22 16:26:47 +0200

    funziona con i prompt

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-// const journeyLenght = parseFloat(prompt('Quanti chilometri vuoi percorrerre?'));
-
+// const journeyLenght = parseFloat(prompt('Quanti chilometri?'));
 // const passengerAge = parseInt(prompt('Quanti anni hai?'));
 
-const price = parseFloat(journeyLenght * 0.21);
-// console.log('Il tuo biglietto viene', price.toFixed(2),'£');
+const price = 0.21 * journeyLenght;
+let discount = 0;
 
 if (passengerAge < 18) {
-    const discount = parseFloat(20 * price / 100);
-    const finalPrice = price - discount;
-    // console.log('I minorenni hanno diritto ad uno sconto del 20%, quindi il tuo biglietto viene', finalPrice.toFixed(2), '£');
+	discount = 20;
 } else if (passengerAge >= 65) {
-    const discount = parseFloat(40 * price / 100);
-    const finalPrice = price - discount;
-    // console.log('Gli over 65 hanno diritto ad uno sconto del 40%, quindi il tuo biglietto viene', finalPrice.toFixed(2), '£');
-} 
+	discount = 40;
+}
+
+let finalPrice = price - price * discount / 100;
+finalPrice = parseFloat(finalPrice);
+
+document.getElementById('ticketPrice').innerHTML = finalPrice.toFixed(2) + '€';
