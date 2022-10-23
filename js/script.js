c2a943856601b92ab1347e6f7d44535bb7237a68
Andrea-Baggio <andreabaggio800@gmail.com>
2022-10-23 12:17:08 +0200

    tasto annulla

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
 const journeyLenght = parseFloat(prompt('Quanti chilometri?'));
 const passengerAge = parseInt(prompt('Quanti anni hai?'));
 
-// const price = 0.21 * journeyLenght;
-// let discount = 0;
-
-// if (passengerAge < 18) {
-// 	discount = 20;
-// } else if (passengerAge >= 65) {
-// 	discount = 40;
-// }
-
-// const finalPrice = price - price * discount / 100;
-// document.getElementById('ticketPrice').innerHTML = finalPrice.toFixed(2) + '€';
-
-
-
 const generator = document.getElementById('btn-gen');
 generator.addEventListener('click', function() {
     console.log('ahia!');
@@ -29,3 +15,25 @@ if (passengerAge < 18) {
 const finalPrice = price - price * discount / 100;
 document.getElementById('ticketPrice').innerHTML = finalPrice.toFixed(2) + '€';
 })
+
+const refreshForm = document.getElementById('refresh');
+refreshForm.addEventListener ('click', function() {
+    window.location.reload(true);
+})
+
+
+
+
+
+
+// const price = 0.21 * journeyLenght;
+// let discount = 0;
+
+// if (passengerAge < 18) {
+// 	discount = 20;
+// } else if (passengerAge >= 65) {
+// 	discount = 40;
+// }
+
+// const finalPrice = price - price * discount / 100;
+// document.getElementById('ticketPrice').innerHTML = finalPrice.toFixed(2) + '€';
