ded6df5968d2704bde7978b447afe297707e50e1
Andrea-Baggio <andreabaggio800@gmail.com>
2022-10-23 11:35:06 +0200

    il tasto "Genera" funziona

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-// const journeyLenght = parseFloat(prompt('Quanti chilometri?'));
-// const passengerAge = parseInt(prompt('Quanti anni hai?'));
+const journeyLenght = parseFloat(prompt('Quanti chilometri?'));
+const passengerAge = parseInt(prompt('Quanti anni hai?'));
 
-const price = 0.21 * journeyLenght;
+// const price = 0.21 * journeyLenght;
+// let discount = 0;
+
+// if (passengerAge < 18) {
+// 	discount = 20;
+// } else if (passengerAge >= 65) {
+// 	discount = 40;
+// }
+
+// const finalPrice = price - price * discount / 100;
+// document.getElementById('ticketPrice').innerHTML = finalPrice.toFixed(2) + '€';
+
+
+
+const generator = document.getElementById('btn-gen');
+generator.addEventListener('click', function() {
+    console.log('ahia!');
+    const price = 0.21 * journeyLenght;
 let discount = 0;
 
 if (passengerAge < 18) {
@@ -9,8 +26,6 @@ if (passengerAge < 18) {
 } else if (passengerAge >= 65) {
 	discount = 40;
 }
-
-let finalPrice = price - price * discount / 100;
-finalPrice = parseFloat(finalPrice);
-
+const finalPrice = price - price * discount / 100;
 document.getElementById('ticketPrice').innerHTML = finalPrice.toFixed(2) + '€';
+})
